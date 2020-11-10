f1eaae3500790787ab4b9f34f3e26eb5dcb88bab
Cosmel Villalobos <cosmelvg@gmail.com>
2020-11-10 18:24:31 -0500

    added the method to catch the initial period for classes

--- bin/main.rb
+++ bin/main.rb
@@ -3,4 +3,8 @@ require_relative '../lib/open'
 require_relative '../lib/checkcomments'
 readit = OpenTheFile.new(ARGV.first)
 commnets = CheckComments.new(readit.open_the_file)
+commnets.initialperiod
 commnets.comments
+
+
+
--- lib/checkcomments.rb
+++ lib/checkcomments.rb
@@ -16,16 +16,29 @@ class CheckComments
     @lines.each_with_index do |line, index|
       if line.include?('/*')
         @errors_array.push(index + 1 )
-        @errors.push("On line #{index + 1} you have this commented code #{line}".red)
+        @errors.push("On line #{index + 1} you have this commented code #{line}".yellow)
       end
     end
     puts @errors
     @errors_array
   end
+
+
+
+  def initialperiod
+    period = /^\./
+    unless @lines[0].match(period)
+        @errors.push("On line 1 of your code you don't have a period".red)
+    end
+  end
+
+
+
 end
 
-# comentarios = CheckComments.new
+# comentarios = CheckComments.new(lines)
 # puts comentarios.comments
+# puts comentarios.initialperiod
 # puts comentarios.displayarray
 # puts comentarios.errors
 
